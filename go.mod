fd8ddb32651001773523aa6be87eac45782592d5
Saufi <msaufi2325@gmail.com>
2024-04-09 10:44:11 +0900

    Add support for loading password from environment variable

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/msaufi2325/Generate_hash_password
+
+go 1.22.1
+
+require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.22.0
+)
--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	password := "password"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	password := os.Getenv("PASSWORD")
 
 	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 12)
 	fmt.Println(string(hashedPassword))
