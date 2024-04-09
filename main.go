b9ec416bc72419c9471d851138843a786c89bcdf
Saufi <msaufi2325@gmail.com>
2024-04-09 10:31:41 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	password := "password"
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 12)
+	fmt.Println(string(hashedPassword))
+}
